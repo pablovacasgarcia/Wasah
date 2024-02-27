8ccbcc9e2f66baf8f11999aca046849fe6621abf
Pablo Vacas García <pablovacasgarcia@gmail.com>
2024-02-27 10:25:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ app.post('/upload', function(req, res) {
     }
   
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    sampleFile = req.files.fichero;
-      const utf8FileName = sampleFile.name;
-  
-      let n_archivo = utf8FileName;
+    sampleFile = req.files.file;
+    const utf8FileName = sampleFile.name;
+    let n_archivo = utf8FileName;
+
     uploadPath = __dirname + '/public/files/' + n_archivo;
   
     // Use the mv() method to place the file somewhere on your server
@@ -43,6 +43,6 @@ app.post('/upload', function(req, res) {
             return res.status(500).json({ error: err });
   
         // Envía un JSON de éxito con un mensaje personalizado
-        res.json({ success: true, message: 'File uploaded successfully!', name: n_archivo});
+        res.json({ success: true, message: 'File uploaded successfully!', nombre: n_archivo});
     })
 })
